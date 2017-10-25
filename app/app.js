c24871992905470788125131b2f9e4362cb101f2
Tiffani <tiffani.grace.elmore@gmail.com>
2017-10-24 19:16:33 -0500

    First commit

---
+++ app/app.js
@@ -0,0 +1,10 @@
+
+import React from 'react'
+import { render } from 'react-dom'
+import { Navbar } from './components/Navbar'
+import { Signup } from './components/Signup'
+
+// render(
+// ,
+// 	document.getElementById('app')
+// )
