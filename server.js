93b556e765068894ff704118b875a1b6f8817e13
Tiffani <tiffani.grace.elmore@gmail.com>
2017-10-26 01:51:21 -0500

    not using .scss ext

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/public/'));
+// app.use(express.static(`${__dirname}/public`));
+app.use(express.static("./public"));
 
-app.listen(process.env.PORT || 8080);
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+// Starting our express server
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
+
